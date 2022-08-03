24112a3d51f558bf5200873e826376a69ab69ee7
Zihong Qu <50311274+ZihongQu@users.noreply.github.com>
2022-08-04 00:07:58 +0930

    Update index.js

--- index.js
+++ index.js
@@ -12,7 +12,7 @@ app.get("/", (req, res) => {
 });
 
 app.get("/a", (req, res) => {
-  res.status(200).json({ message: "Hello world plsssssss" });
+  res.status(200).json({ message: "Hello world plsssssssss" });
 });
 
 app.listen(5001, () => {
