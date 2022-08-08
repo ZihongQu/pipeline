86d3bc1986b2aad487168c676f9d58e3dc9f4a86
zihong <zihong@splose.com>
2022-08-08 12:34:19 +0930

    update

--- index.js
+++ index.js
@@ -8,7 +8,7 @@ app.use(bodyParser.urlencoded({ limit: "30mb", extended: true }));
 app.use(cors());
 
 app.get("/", (req, res) => {
-  res.status(200).json({ message: "Hello world !!!" });
+  res.status(200).json({ message: "Hello worl" });
 });
 
 app.listen(5001, () => {
